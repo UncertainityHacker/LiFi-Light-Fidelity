eb439049542746bcdbb72510d16266f78f8116c8
UncertainityHacker <44387477+UncertainityHacker@users.noreply.github.com>
2018-10-23 14:15:49 +0530

    Add files via upload

---
+++ Lifi_Channel_1/Lifi_Channel_1.ino
@@ -0,0 +1,40 @@
+/* 
+   This code is for one end of the two way communication using lifi
+   In this code we made use of AltSoftSerial Library so that we can 
+   create both transmitting and recieving end simultaneously.
+/****************************************************************************************************************************************************************************************/
+
+#include <AltSoftSerial.h>              //including the library for communication channel for both the ends
+
+AltSoftSerial recieving;                // creating instance for recieving the text
+AltSoftSerial sending;                  // creating instance for sending the text
+
+char x;
+String outgoing;                        //declaring the string variable for message to be sent
+String incoming="";                     //declaring the string variable for message to be sent
+void setup() {
+  
+Serial.begin(9600);                     //starting the serial communication
+recieving.begin(800);                   //starting the recieving channel
+sending.begin(400);                     //starting the transmission channel
+}
+
+void loop() 
+{
+  if(recieving.available()!=0)    //waiting till someting is recieved
+  {           
+  Serial.println("Recieving the message:");
+  incoming = recieving.readString(); 
+  Serial.print(incoming);               //printing the recieved message on serial monitor
+  Serial.flush();
+  }
+ 
+  if(Serial.available()!=0)     //waiting for user to give the input
+  {
+  Serial.println("Sending the message:");
+  outgoing = Serial.readString();
+  sending.print(outgoing);
+  Serial.print(outgoing);                //printing the message to send on serial monitor
+  Serial.flush();
+  }
+}
---
+++ Lifi_Channel_2/Lifi_Channel_2.ino
@@ -0,0 +1,37 @@
+#include <AltSoftSerial.h>              //including the AltSoftSerial library for setting a two way communication
+AltSoftSerial sending;                  // creating instance for recieving the text
+AltSoftSerial recieving;                 // creating instance for sending the text
+
+String incoming="";                     //declaring the string variable for message to be sent
+String outgoing="";                     //declaring the string variable for message to be sent
+void setup() 
+{
+   Serial.begin(9600);                     //starting the serial communication
+  sending.begin(800);                     //starting the recieving channel
+  recieving.begin(400);                   //starting the transmission channel
+}
+
+void loop() 
+{
+  if(Serial.available()!=0)       //waiting till someting is recieved
+  {
+  Serial.println("Enter the message");
+  incoming=Serial.readString();
+  Serial.print(incoming);                //printing the recieved message on serial monitor
+  sending.print(incoming);
+  Serial.flush();
+  }
+  
+  if(recieving.available()!=0)    //waiting for user to give the input
+  {
+  Serial.println("recieving the message");
+  outgoing=recieving.readString();
+  if(outgoing=="")
+  {
+    sending.print("Message not sent");
+    Serial.flush();
+  }
+  Serial.print(outgoing);                 //printing the message to send on serial monitor
+  Serial.flush();
+  }
+}
